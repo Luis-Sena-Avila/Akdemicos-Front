f2023a4c596b130d3e428e34c74689d6f4af5d56
Luis-Sena-Av <akdemicos721@gmail.com>
2024-12-20 11:16:01 -0500

    Nuevo

--- src/Pages/CalculoDiferencial.jsx
+++ src/Pages/CalculoDiferencial.jsx
@@ -1,8 +1,21 @@
+import "../Styles/CalculoDiferencial.css"
+import React from "react"
+
 const CalculoDiferencial = () => {
     
     return (
-      <div className='home'>
-          H
+      <div className='ContainerPrincipal'>
+          <div className="indice">
+            <h3>Contenido</h3>
+          </div>
+
+          <div className="contenido">
+            <h2 className="titlePrincipal">Calculo Diferencial</h2>
+            <h3 className="capitulo">Funciones</h3>
+            <p className="texto">Una función es una relación en la que a cada elemento <img src="/public/x.svg" alt="x" />  de un conjunto llamado Dominio le corresponde uno y sólo un
+            elemento llamado <img className="imagen" src="/public/fx.svg" alt="f(x)" /> de otro conjunto llamado Rango.</p>
+          
+          </div>
       </div>
     )
   }
